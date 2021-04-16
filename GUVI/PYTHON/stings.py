7231e96dbc9bd8aeddf900751d7fac993c33911e
Kiranarul <kiranarul2000@gmail.com>
2021-04-16 10:35:46 +0530

    new update

---
+++ GUVI/PYTHON/stings.py
@@ -0,0 +1,40 @@
+x='''This is a 
+multi
+line 
+string'''
+
+print(x)
+
+a="Hello world"
+print(a[0])
+
+print(a[1:5])
+
+print(a[-5:-2])
+
+print(a[::-1])
+
+b="heli pad      "
+
+print(b)
+print(b.strip())
+
+print("heli" in b)
+
+print(b.upper())
+
+print(b.lower())
+
+print(b.replace("heli","hello"))
+
+print(b.split())
+print(b.split(","))
+print(b.split("p"))
+print(b.strip().split("p"))
+
+print(a+"    "+b)
+
+a="12345678"
+print(a[2:5])
+#Google - String Methods
+#Google - Escape Characters
